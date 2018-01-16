9f8854e0c95a0a8b1fdf11f663023a3be497ec54
ZENG <zeng@ZENGdeiMac.lan>
2018-01-16 18:01:14 +0800

    fix

--- resources/views/user/login.blade.php
+++ resources/views/user/login.blade.php
@@ -40,6 +40,7 @@
 @if (session('status'))
 {{--    {{dd(session('status'))}}--}}
     <script type="text/javascript">
+        console.log({{session('status')}})
         alert("{{session('status')}}")
     </script>
 
